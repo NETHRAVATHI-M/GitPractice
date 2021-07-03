8899c55cf9d886fb76fab3c8f5f679efa66db556
nethr <nethr@DESKTOP-74L7PJ6>
2021-07-03 22:06:37 +0530

    connnection added

--- src/GitRepo2.java
+++ src/GitRepo2.java
@@ -4,7 +4,7 @@ public class GitRepo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git repository");
-		System.out.println("added comment accodring to pull request");
+		System.out.println("added comment accodring to pull request");System.out.println("hello");
 	}
 
 }
